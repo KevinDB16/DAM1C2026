30f5667620c2b82545cd0dbbd95447b33ac33adb
Kevin <kevin.delbello@bue.edu.ar>
2026-05-07 17:10:41 -0300

    Primer commit

---
+++ app/src/main/java/com/example/primerproyectodam2026/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.primerproyectodam2026
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnIrMenu = findViewById<Button>(R.id.btnIrMenu)
+        btnIrMenu.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
